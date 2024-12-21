cc169abc945483e1e5deba08530c721afd73cc0e
Heloísa Raquel Siebeneichler <124800380+heloisars2@users.noreply.github.com>
2024-12-21 14:50:30 -0300

    Update tb_contador.vhdl

--- Contador/tb_contador.vhdl
+++ Contador/tb_contador.vhdl
@@ -11,21 +11,39 @@ architecture contar of tb_contador
 
     component contador is
         port(
-            clk: in std_logic;
-            reset in std_logic;
-            clear in std_logic;
+            clk, reset, pr: in std_logic;
             q_cont : out std_logic_vector(2 downto 0);
     
         );
     end component
 
-    signal sj, sk, spr, scl, sq, snq : std_logic;
+    component controler is
+        port(
+            q : in std_logic_vector (2 downto 0);
+            j,k : out std_logic_vector (2 downto 0);
+            
+        );
+    end component;
+
+    signal spr, scl, sreset : std_logic;
     signal sclk : std_logic := '1';
     signal s_qcont : std_logic_vector (2 downto 0);
-    begin
-    --instancia de JK e port map
-        u_ffjk: ffjk port map(sj,sk,sclk,spr,scl,sq,snq);
-    --process
+    
+    contador_u:contador port map (sclk, sreset, spr, s_qcout) 
+
+    u_contador: process 
+    begin 
+    spr <= '1';
+    sreset <= '0';
+    wait for CLK_PERIOD; 
+    sreset <= '1';
+    wait for CLK_PERIOD; 
+    end process
+    p_clock_n : process
+        begin
+            s_clk <= not(s_clk);
+            wait for CLK_PERIOD/2;
+        end process;
     end architecture
     
 
